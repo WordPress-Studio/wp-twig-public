8b972cbb3884647f2196e78388b7718ec5a24114
Prosenjit Manna <prosenjit@itobuz.com>
2021-07-04 22:46:00 +0530

    Remove  unused code

--- wp-content/themes/wp-twig/functions.php
+++ wp-content/themes/wp-twig/functions.php
@@ -85,29 +85,6 @@ function wp_twig_block_category( $categories, $post ) {
 }
 
 
-
-/**
- * This ensures that Timber is loaded and available as a PHP class.
- * If not, it gives an error message to help direct developers on where to activate
- */
-if ( ! class_exists( 'Timber' ) ) {
-
-	add_action(
-		'admin_notices',
-		function() {
-			echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
-		}
-	);
-
-	add_filter(
-		'template_include',
-		function( $template ) {
-			return get_stylesheet_directory() . '/static/no-timber.html';
-		}
-	);
-	return;
-}
-
 /**
  * Sets the directories (inside your theme) to find .twig files
  */
